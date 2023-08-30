f5fcf1a64d601a61a3bd9f93f9ea939c5032cbc0
Amira Abdulle Iid <amira.abdulleiid@elev.ga.nitg.se>
2023-08-30 10:38:28 +0200

    initial commit

---
+++ Project #2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+
+string name = "";
+string answer = "";
+string response = "";
+string questionAnswer = "";
+
+Console.WriteLine("what's your name?");
+
+name = Console.ReadLine();
+ 
+Console.WriteLine($"Your name is {name}?, dina föräldrar måste hata dig.");
+
+answer = Console.ReadLine();
+
+Console.WriteLine($"Snabb fråga {name}, whos the queen of rap?");
+
+response = Console.ReadLine();
+
+Console.WriteLine($"ärligt så kan jag inte läsa om {response} var rätt eller fel. anyways finish the lyrics in 1 sentence. Justin Bieber You know I'mma hit 'em with the ether Buns out, weiner.");
+
+questionAnswer = Console.ReadLine();
+
+Console.WriteLine($"BEAUTY, BEAUTY AND A BEAST. BEAUTY FROM THE STREETS. BEAUTIGUL CONFESSIONS AND A PREIST, BEAST. BEAUTY FROM THE STREETS, WE DONT GET DECEASED EVERYTIME BEAUTY ON THE BEAT EATS BODY ROCK OH");
+
+Console.ReadLine();
